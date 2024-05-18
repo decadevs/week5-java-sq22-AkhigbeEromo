ea3e86e7b48ad4b1ab94183385da298f52b430e8
EROMOSELE AKHIGBE <akhigbeeromo@gmail.com>
2024-05-18 11:22:57 +0100

    Completed structure for project

---
+++ Assignment/src/main/java/models/Person.java
@@ -0,0 +1,50 @@
+package models;
+
+import enums.Gender;
+import enums.Role;
+
+public class Person {
+    private String name;
+    private int age;
+    private Gender gender;
+    private Role role;
+
+    public Person(String name, int age, Gender gender, Role role) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+        this.role = role;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public Gender getGender() {
+        return gender;
+    }
+
+    public void setGender(Gender gender) {
+        this.gender = gender;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+}
